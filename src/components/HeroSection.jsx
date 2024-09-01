144f33f3fbdbae815857488accfa9d17221a3f84
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-09-01 14:59:51 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,18 +18,17 @@ const HeroSection = () => {
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 IT training institute</span>
-                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-blue-600'>Dream Jobs</span></h1>
-                <p>seeree is one of the best IT training institute and Software industry.</p>
-                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+                <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 IT training institute</span>
+                <h1 className='text-3xl md:text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-blue-600'>Dream Jobs</span></h1>
+                <p>seeree is one of the best IT training institutes and Software industry.</p>
+                <div className='flex w-full max-w-md md:max-w-lg lg:max-w-xl shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
                         placeholder='Find your dream jobs'
                         onChange={(e) => setQuery(e.target.value)}
-                        className='outline-none border-none w-full'
-
+                        className='outline-none border-none w-full px-2 py-2 rounded-l-full'
                     />
-                    <Button onClick={searchJobHandler} className="rounded-r-full bg-blue-600">
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-blue-600 px-4 py-2">
                         <Search className='h-5 w-5' />
                     </Button>
                 </div>
@@ -38,4 +37,4 @@ const HeroSection = () => {
     )
 }
 
-export default HeroSection
+export default HeroSection;
