2278f2f61aaf0a6d98e968f2e7cc2ea3fabff334
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-11-14 07:54:37 +0530

    add

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
               <PopoverTrigger asChild>
                 <Avatar className="cursor-pointer">
                   <AvatarImage
-                    src={user?.profile?.profilePhoto}
+                    src={user?.profile?.profilePhoto||'https://i.pinimg.com/564x/94/c6/25/94c62511d312b8612aa0ab92318966f2.jpg'}
                     alt="@shadcn"
                   />
                 </Avatar>
